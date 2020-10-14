c3a4926b701cba445bcc0b23bca1ffccf1d6fcbb
DmanCoder <ktdenis.1@live.com>
2020-10-14 15:52:25 +1000

    Added basic skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,133 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="intro">
+        <div class="content">
+          <h1>Pen Animation</h1>
+          <p>
+            Learn how to reveal parts of a product or uncover your story step by
+            step.
+          </p>
+          <p>
+            <img src={require('./img/img_pen-landscape.png')} alt="" />
+          </p>
+        </div>
+      </div>
+
+      <div id="main" class="main-container">
+        <section class="section-static bcg-white">
+          <div class="wrapper">
+            <div class="copy">
+              <h2>Static Section</h2>
+
+              <p class="subtitle">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                in dui mauris. Vivamus hendrerit arcu sed erat molestie
+                vehicula.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section id="reveal-effect" class="section-static bcg-blue">
+          <div class="wrapper">
+            <div class="copy">
+              <h2>Reveal Effect</h2>
+
+              <p class="subtitle">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                in dui mauris. Vivamus hendrerit arcu sed erat molestie
+                vehicula.
+              </p>
+            </div>
+
+            <div class="parts">
+              <div class="part part1">
+                <h3>Heading 1</h3>
+                <div class="image"></div>
+                <p>
+                  Vivamus hendrerit in dui arcu sed erat molestie vehicula.
+                  Nullam in dui mauris.
+                </p>
+              </div>
+              <div class="part part2">
+                ``
+                <h3>Heading 2</h3>
+                <div class="image"></div>
+                <p>
+                  Vivamus hendrerit in dui arcu sed erat molestie vehicula.
+                  Nullam in dui mauris.
+                </p>
+              </div>
+              <div class="part part3">
+                <h3>Heading 3</h3>
+                <div class="image"></div>
+                <p>
+                  Vivamus hendrerit in dui arcu sed erat molestie vehicula.
+                  Nullam in dui mauris.
+                </p>
+              </div>
+              <div class="part part4">
+                <h3>Heading 4</h3>
+                <div class="image"></div>
+                <p>
+                  Vivamus hendrerit in dui arcu sed erat molestie vehicula.
+                  Nullam in dui mauris.
+                </p>
+              </div>
+              <div class="part part5">
+                <h3>Heading 5</h3>
+                <div class="image"></div>
+                <p>
+                  Vivamus hendrerit in dui arcu sed erat molestie vehicula.
+                  Nullam in dui mauris.
+                </p>
+              </div>
+              <div class="part part6">
+                <h3>Heading 6</h3>
+                <div class="image"></div>
+                <p>
+                  Vivamus hendrerit in dui arcu sed erat molestie vehicula.
+                  Nullam in dui mauris.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="section-static bcg-white">
+          <div class="wrapper">
+            <div class="copy">
+              <h2>Static Section</h2>
+
+              <p class="subtitle">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                in dui mauris. Vivamus hendrerit arcu sed erat molestie
+                vehicula.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div class="footer-container">
+        <footer class="wrapper">
+          <h3>
+            2020 &copy;
+            <a href="https://ihatetomatoes.net" target="_blank">
+              Ihatetomatoes.net
+            </a>
+            |
+            <a href="https://twitter.com/ihatetomatoes" target="_blank">
+              @ihatetomatoes
+            </a>
+            | Animate responsibly!
+          </h3>
+        </footer>
+      </div>
     </div>
   );
 }
